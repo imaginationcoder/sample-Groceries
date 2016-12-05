a89d4c96fea4cc76af150891cf4d1221c0981f78
Upender Devulapally <upender.devulapally@gmail.com>
2016-12-05 19:56:34 +0530

    Adding User model and sepaerating template to login.html

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,28 +1,22 @@
 import { Component } from "@angular/core";
 
-@Component({
-  selector: "my-app",
-  template: `
-   <StackLayout>
-   <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
-    <TextField hint="Email Address" keyboardType="email"
-      autocorrect="false" autocapitalizationType="none" [(ngModel)]="email"></TextField>
-    <TextField hint="Password" secure="true"></TextField>
+import { User } from "./shared/user/user";
 
-    <Button [text]="isLoggingIn ? 'Sign in' : 'Sign up'" class="submit-button" (tap)="submit()"></Button>
-    <Button [text]="isLoggingIn ? 'Sign up' : 'Back to login'" (tap)="toggleDisplay()"></Button>
-   </StackLayout>
-  `,
+@Component({
+    selector: "my-app",
+    templateUrl: "pages/login/login.html",
     styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 export class AppComponent {
-    email = "nativescriptrocks@telerik.com";
+    user: User;
     isLoggingIn = true;
-    submit() {
-        console.log("hello1234");
-        alert("You’re using: " + this.email);
-    }
 
+    constructor() {
+        this.user = new User();
+    }
+    submit() {
+        alert("You’re using: " + this.user.email);
+    }
     toggleDisplay() {
         this.isLoggingIn = !this.isLoggingIn;
     }
