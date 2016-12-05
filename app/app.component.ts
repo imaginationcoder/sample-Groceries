ec29b8e828fad2fefefc3b23e8f39c0449251c9e
Upender Devulapally <upender.devulapally@gmail.com>
2016-12-05 12:50:26 +0530

    Stack Layout with  TextField and Button UI elements

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,15 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "my-app",
-  template: "<Label text='hello world'></Label>"
+  template: `
+   <StackLayout>
+    <TextField hint="Email Address" keyboardType="email"
+      autocorrect="false" autocapitalizationType="none"></TextField>
+    <TextField hint="Password" secure="true"></TextField>
+
+    <Button text="Sign in"></Button>
+    <Button text="Sign up for Groceries"></Button>
+   </StackLayout>
+  `
 })
 export class AppComponent {}
