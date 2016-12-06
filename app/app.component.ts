2876d285b54a723c51e7a4e59e91533fec48cf0f
Upender Devulapally <upender.devulapally@gmail.com>
2016-12-06 18:44:25 +0530

    added application logic for signup

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
 
 import { User } from "./shared/user/user";
+import { UserService } from "./shared/user/user.service";
 
 @Component({
     selector: "my-app",
+    providers: [UserService],
     templateUrl: "pages/login/login.html",
     styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
@@ -11,13 +13,36 @@ export class AppComponent {
     user: User;
     isLoggingIn = true;
 
-    constructor() {
+    constructor(private userService: UserService) {
         this.user = new User();
+        this.user = new User();
+        this.user.email = "user@nativescript.org";
+        this.user.password = "password";
     }
-    submit() {
-        alert("You’re using: " + this.user.email);
-    }
+
     toggleDisplay() {
         this.isLoggingIn = !this.isLoggingIn;
+    }
+
+    submit() {
+        if (this.isLoggingIn) {
+            this.login();
+        } else {
+            this.signUp();
+        }
+    }
+    login() {
+        // TODO: Define
+    }
+
+    signUp() {
+        this.userService.register(this.user)
+            .subscribe(
+            () => {
+                alert("Your account was successfully created.");
+                this.toggleDisplay();
+            },
+            () => alert("Unfortunately we were unable to create your account.")
+        );
     }
 }
